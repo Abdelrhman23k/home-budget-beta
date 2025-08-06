ab22ea4b747ac86b66738b409100174bfda560b4
Abdelrhman23k <71826691+Abdelrhman23k@users.noreply.github.com>
2025-08-06 17:01:06 +0300

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,3 @@
+// This file handles all DOM manipulation, rendering, and UI event listeners.
+// It is the largest file and acts as the "view" layer of the application.
+// ... (Full code for ui.js will be in the next response)
