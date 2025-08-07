4889a0b52ac3990abb4ca30a4f2c530bd712dd28
Abdelrhman23k <71826691+Abdelrhman23k@users.noreply.github.com>
2025-08-07 11:52:25 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,7 +3,6 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { firebaseConfig } from './config.js';
 
-// Initialize Firebase and export the core services
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
